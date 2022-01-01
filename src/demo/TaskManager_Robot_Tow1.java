ffbd2578629d5769fc59866c0f14649c07ffd498
DonggiKim <7979us@gmail.com>
2022-01-01 15:01:47 +0900

    tow robot plan fixed

--- src/demo/TaskManager_Robot_Tow1.java
+++ src/demo/TaskManager_Robot_Tow1.java
@@ -4,7 +4,7 @@ import taskManager.TaskManager_Robot;
 
 public class TaskManager_Robot_Tow1 {
 	public static void main(String[] args) {
-		TaskManager_Robot robot = new TaskManager_Robot("AMR_TOW1", "172.16.165.171", 61114);
+		TaskManager_Robot robot = new TaskManager_Robot("AMR_TOW1", "172.16.165.222", 61114);
 		
 	}
 }
