5fbd16db292f73f32bb7ee04f2ad499b5628e6e5
DonggiKim <7979us@gmail.com>
2022-01-01 21:16:47 +0900

    remove log

--- src/demo/TaskManager_Robot_Lift1.java
+++ src/demo/TaskManager_Robot_Lift1.java
@@ -4,7 +4,7 @@ import taskManager.TaskManager_Robot;
 
 public class TaskManager_Robot_Lift1 {
 	public static void main(String[] args) {
-		TaskManager_Robot robot = new TaskManager_Robot("AMR_LIFT1", "172.16.165.222", 61116);
+		TaskManager_Robot robot = new TaskManager_Robot("AMR_LIFT1", "172.16.165.106", 61116);
 		
 	}
 }
--- src/demo/TaskManager_Robot_Tow2.java
+++ src/demo/TaskManager_Robot_Tow2.java
@@ -4,7 +4,7 @@ import taskManager.TaskManager_Robot;
 
 public class TaskManager_Robot_Tow2 {
 	public static void main(String[] args) {
-		TaskManager_Robot robot = new TaskManager_Robot("AMR_TOW2", "172.16.165.222", 61412);
+		TaskManager_Robot robot = new TaskManager_Robot("AMR_TOW2", "172.16.165.106", 61412);
 		
 	}
 }
